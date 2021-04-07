d25923c91cf4e2a98dd1cbc13db3a9ddc923f20d
Patrik Garten <info@patrikgarten.de>
2021-04-07 08:01:25 +0200

    feat: finalise layout

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,11 +1,15 @@
 document.querySelectorAll(".frame-type-pgae_fancycard").forEach((item) => {
   const headerElement = item.querySelector(".fancycard__header");
 
+  item.addEventListener("click", () => {
+    if (window.innerWidth > 992) {
+      item.firstChild.setAttribute("data-toggle", "modal")
+    } else {
+      item.firstChild.removeAttribute("data-toggle")
+    }
+  });
+
   if (headerElement) {
-    headerElement.firstChild.style.background = headerElement.getAttribute("data-color");
-
-
-
     const btnToggle = headerElement.querySelector(".btn-toggle")
     if (btnToggle) {
       btnToggle.addEventListener("click", () => {
