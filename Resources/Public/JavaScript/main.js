f5f71f3c9f869ea194d39d36248e438895c096e5
Patrik Garten <info@patrikgarten.de>
2021-04-12 17:16:10 +0200

    feat: change display behaviour

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,21 +1,28 @@
 document.querySelectorAll(".frame-type-pgae_fancycard").forEach((item) => {
-  const headerElement = item.querySelector(".fancycard__header");
 
   item.addEventListener("click", () => {
-    if (window.innerWidth > 992) {
+    if (window.innerWidth > 768) {
       item.firstChild.setAttribute("data-toggle", "modal")
     } else {
       item.firstChild.removeAttribute("data-toggle")
     }
   });
 
+  const headerElement = item.querySelector(".fancycard__header");
   if (headerElement) {
     const btnToggle = headerElement.querySelector(".btn-toggle")
     if (btnToggle) {
       btnToggle.addEventListener("click", () => {
         const bodyElement = headerElement.parentElement.querySelector(".fancycard__body");
-        bodyElement.classList.toggle("hidden");
+        bodyElement.classList.toggle("d-none");
       });
     }
   }
+
+  const color = item.getAttribute("data-body-title-color");
+  item.querySelectorAll(".fancycard__body").forEach((element) => {
+    element.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((header) => {
+      header.style.color = color;
+    })
+  })
 });
