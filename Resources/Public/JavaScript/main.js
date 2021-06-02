e00c63cb3ce527a7e525a1bd954f4c486fa57b19
Andreas Riermeier <riermeier@welovebold.de>
2021-06-02 13:53:19 +0200

    Changes responsive mobile

--- Resources/Public/JavaScript/main.js
+++ Resources/Public/JavaScript/main.js
@@ -1,21 +1,33 @@
 document.querySelectorAll(".frame-type-pgae_fancycard").forEach((item) => {
-  const headerElement = item.querySelector(".fancycard__header");
 
   item.addEventListener("click", () => {
-    if (window.innerWidth > 992) {
+    if (window.innerWidth > 767) {
       item.firstChild.setAttribute("data-toggle", "modal")
     } else {
       item.firstChild.removeAttribute("data-toggle")
     }
   });
 
+  const headerElement = item.querySelector(".fancycard__header");
   if (headerElement) {
     const btnToggle = headerElement.querySelector(".btn-toggle")
     if (btnToggle) {
       btnToggle.addEventListener("click", () => {
         const bodyElement = headerElement.parentElement.querySelector(".fancycard__body");
-        bodyElement.classList.toggle("hidden");
+        document.querySelectorAll(".frame-container .fancycard__body").forEach((item) => {
+          if(!item.classList.contains("d-none") && item !== bodyElement){
+            item.classList.add("d-none");
+          }
+        });
+        bodyElement.classList.toggle("d-none");
       });
     }
   }
+
+  const color = item.getAttribute("data-body-title-color");
+  item.querySelectorAll(".fancycard__body").forEach((element) => {
+    element.querySelectorAll("h1, h2, h3, h4, h5, h6").forEach((header) => {
+      header.style.color = color;
+    })
+  })
 });
